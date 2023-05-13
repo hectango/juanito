b8aa95bb1d83055f20164e548a168f604198f453
Juan Castellon <jcc.eth@gmail.com>
2023-05-13 16:05:36 +0100

    Initial contract and test

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const Token = await ethers.getContractFactory("Token");
+    // uint256 _initialAmount, string memory _tokenName, uint8 _decimalUnits, string  memory _tokenSymbol)
+    const token = await Token.deploy(10000000000000, "Hectango", 18, "HTG");
+  
+    console.log("Token address:", token.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
